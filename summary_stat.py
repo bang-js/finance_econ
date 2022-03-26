31fd9c4d2f5e65cf340b850fd8e45dba04089734
bang-js <95086336+bang-js@users.noreply.github.com>
2022-03-26 14:01:18 +0900

    Update summary_stat.py

--- summary_stat.py
+++ summary_stat.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-# 요약통계량 계산
+# 요약통계량 계산 (count, mean, std, min, 25%, med, 75%, max)
 df[["A","B"]].describe()
 
 # mean
 df[["A","B"]].mean()
+
+# std
+df[["A","B"]].std()
+
+# skewness
+# kurtosis
+
+# 정규성 검정
