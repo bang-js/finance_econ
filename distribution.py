faa83bd5a55dbd9a22ca825bb2b4a79b49489590
bang-js <95086336+bang-js@users.noreply.github.com>
2022-03-30 00:10:31 +0900

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -1,8 +1,8 @@
 import math
 
-###
+#####
 # POISSON DIST
-###
+#####
 
 lambd = int(input("lambda값:"))
 
@@ -10,5 +10,15 @@ def poisson_pdf(x):
     poisson_pdf_value = math.exp((-1)*lambd) * pow(lambd,x) / math.factorial(x)
     return poisson_pdf_value
 
+def poisson_cdf(x):
+    poisson_cdf_value = 0
+    for i in range(x+1) :
+        poisson_cdf_value += math.exp((-1)*lambd) * pow(lambd,i) / math.factorial(i)
+    return poisson_cdf_value
+
 for i in range(10) :
     print("x={}".format(i), round(poisson_pdf(i),8))
+    
+print("cdf")
+for i in range(10) :
+    print("x={}".format(i), round(poisson_cdf(i),8))
