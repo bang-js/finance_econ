fe119a5049f976057f2db69c573bc6ee1d547dbc
bang-js <95086336+bang-js@users.noreply.github.com>
2022-04-21 22:07:59 +0900

    Create DSGE.py

---
+++ DSGE.py
@@ -0,0 +1,14 @@
+# https://github.com/kerkphil/DSGE-Utilities
+# https://notes.quantecon.org/submission/5b3ec499b9eab00015b89f97
+
+######
+# DSGE MODELLING
+######
+
+# paramaters setting
+# utility function
+# production function
+# government policy : r, t, g
+
+# Bellman Eq
+# Envelope Thm
